29c50aaf1eb5c784088f08e75433235eb8418a15
elisa181205 <elisandradsantos@gmail.com>
2022-10-14 15:44:26 -0300

    instruções git

---
+++ comandos_git.js
@@ -0,0 +1,22 @@
+ // git init -> Inicia o repositório localmente;
+
+ // git add . -> Adiciona todos os arquivos/pasta no repositório local;
+
+ // git commit -m "mensagem" -> Cria um commit/cópia dos arqiovos/pastas no repositório local;
+
+ /* git remote add nome_qualquer https://github.com/Elisa181205/caminho_repositório_github -> Atribui o caminho especificado
+ para o nome indicado ("nome_qualquer"); */
+
+
+/* ---------------- PRIMEIRO PUSH  ---------------- */
+
+/* ---- A branch nada mais é do que uma espécie de "universo" dentro do repositório ---- */
+
+// git branch -m nome_branch -> Muda/cria a branch especificada;
+
+// git push -u nome_qualquer nome_branch -> envia os arquivos/pastas do repositório local, para o repositório externo;
+
+
+/* ---------------- PUSHES QUAISQUER --------------- */
+
+// git push -> envia os arquivos/pastas do repositório local, para o repositório externo;
